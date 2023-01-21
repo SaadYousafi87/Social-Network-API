5ae0938328346fa2887da98c948950b030610d14
saadyousafi87 <saad.yousafi87@gmail.com>
2023-01-21 18:21:42 -0500

    fixed a small typo

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,7 +11,7 @@ const {
 router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId'),get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 
 
